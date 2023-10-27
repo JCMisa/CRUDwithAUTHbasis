9df4cb4f2a0b78f44d02d9f50d974963857cee42
JC MISA <johncarlomisa399@gmail.com>
2023-10-27 01:33:35 -0700

    Add project files.

---
+++ CRUDwithAUTHbasis/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDwithAUTHbasis.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int salary { get; set; }
+    }
+}
---
+++ CRUDwithAUTHbasis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDwithAUTHbasis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
